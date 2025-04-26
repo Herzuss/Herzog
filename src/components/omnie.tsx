265051c96a660d1b264ca8a0679e573ebb4f6b47
Filip Herzog <filip.herzog05@gmail.com>
2025-04-27 01:11:04 +0200

    update zdjec

--- src/components/omnie.tsx
+++ src/components/omnie.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import zdjecie from "@/../public/images/OMNIE/moja fotka.svg";
+import zdjecie from "@/../public/images/OMNIE/moja fotka.jpg";
 import ikonki from "@/../public/images/OMNIE/ikonki.svg";
 import tlo from "@/../public/images/OMNIE/tlo.svg";
 
